ac61427b9846b1345abab696221d3645c50d20a6
fgsqqq123 <fgsqqq123@gmail.com>
2024-11-27 14:59:19 +0800

    init repo

---
+++ src/main/java/org/example/base_local/BaseLocalApplication.java
@@ -0,0 +1,13 @@
+package org.example.base_local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseLocalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/base_local/BaseLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.base_local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
